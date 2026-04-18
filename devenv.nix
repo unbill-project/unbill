a4803cd30a343c5e16ca878824b88611c27fd14d
Patrick Li <baichuanli@yahoo.com>
2026-04-18 18:50:51 -0400

    ci: devenv

---
+++ devenv.nix
@@ -0,0 +1,32 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  # https://devenv.sh/languages/
+  languages.rust = {
+    enable = true;
+    components = [
+      "rustc"
+      "cargo"
+      "clippy"
+      "rustfmt"
+      "rust-analyzer"
+    ];
+  };
+
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs.cargo-tauri
+    pkgs.glib
+    pkgs.atkmm
+    pkgs.pango
+    pkgs.gdk-pixbuf
+    pkgs.gtk3
+    pkgs.webkitgtk_4_1
+  ];
+
+  # See full reference at https://devenv.sh/reference/options/
+}
